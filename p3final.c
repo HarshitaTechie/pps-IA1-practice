60aedd4f3fe933a92027597bace6908729b9d446
HarshTechie <97089353+HarshitaTechie@users.noreply.github.com>
2022-02-06 07:01:45 +0000

    P3Final

---
+++ p3final.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int input_n();
+int sum_n(int n);
+void output(int n,int sum);
+int main();
+
+int input_n()
+{
+  int n;
+  printf("Enter number of values to add\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int sum_n(int n)
+{
+  int sum=0,i=0;
+  for(i=0;i<=n;i++)
+  sum=sum+i;
+  return sum;
+}
+
+void output(int n, int sum)
+{
+  int i;
+  for(i=1;i<n;i++)
+  printf(" %d +",i);
+  printf(" %d is %d",n,sum);
+}
+
+int main()
+{
+  int n,s;
+  n=input_n();
+  s=sum_n(n);
+  output(n,s);
+}
