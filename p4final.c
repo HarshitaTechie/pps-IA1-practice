bd66160896f7641c7d06f195fb342be6d1a87f9a
HarshTechie <97089353+HarshitaTechie@users.noreply.github.com>
2022-02-06 07:36:54 +0000

    p4Final

---
+++ p4final.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int inout_array_size();
+void input_array(int n, int a[n]);
+int sum_n_arrays(int n, int a[n]);
+void out_put(int n, int a[n], int sum);
+int main();
+
+int input_array_size()
+{
+  int n;
+  printf("Enter the number of values to add\n");
+  scanf("%d",&n);
+  return n;
+}
+
+void input_array(int n, int a[n])
+{
+  int i;
+  printf("Enter values to add\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+}
+
+int sum_n_arrays(int n, int a[n])
+{
+  int sum=0,i=0;
+  for(i=0;i<n;i++)
+  {
+    sum+=a[i];
+  }
+  return sum;
+}
+
+void out_put(int n, int a[n], int sum)
+{
+  int i;
+  for(i=0;i<n-1;i++)
+  {
+    printf("%d + ",a[i]);
+  }
+  printf("%d is %d",a[i],sum);
+}
+
+int main()
+{
+  int n,sum; 
+  n=input_array_size();
+  int s[n];
+  input_array(n,s);
+  sum=sum_n_arrays(n,s);
+  out_put(n,s,sum);
+}
